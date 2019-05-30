ef38e4ee58e63d34e5e05bf593ece0625413541d
Cindy Chen <chen_cx0128@hotmail.com>
2019-05-30 19:25:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,16 @@
 def using_push(array, string)
   array.push(string)
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with_args(array)
+  item1 = array.pop
+  item2 = array.pop
+end
